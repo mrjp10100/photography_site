0a3f81e93c9783d3a9df638ada17450465300ad6
CodeWithJP <111375223+mrjp10100@users.noreply.github.com>
2024-11-02 22:11:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+
+	
+	//About page
+	
+	$(".about-text").on('click', function () {
+		$("body").addClass("about-on");
+	});
+	$(".about-close").on('click', function () {
+		$("body").removeClass("about-on");
+	});
+
+	
+	//Contact page
+	
+	$(".contact-text").on('click', function () {
+		$("body").addClass("contact-on");
+	});
+	$(".contact-close").on('click', function () {
+		$("body").removeClass("contact-on");
+	});
+
+	
+	//Travel portfolio page
+	
+	$(".travel").on('click', function () {
+		$("body").addClass("travel-on");
+	});
+	$(".travel-close").on('click', function () {
+		$("body").removeClass("travel-on");
+	});
+
+	
+	//Wildlife portfolio page
+	
+	$(".wildlife").on('click', function () {
+		$("body").addClass("wildlife-on");
+	});
+	$(".wildlife-close").on('click', function () {
+		$("body").removeClass("wildlife-on");
+	});
+
+	
+	//Nature portfolio page
+	
+	$(".nature").on('click', function () {
+		$("body").addClass("nature-on");
+	});
+	$(".nature-close").on('click', function () {
+		$("body").removeClass("nature-on");
+	});
+
+	
+})(jQuery);
+
+// Get the modal element
+const modal = document.getElementById("imageModal");
+
+// Get the modal image element
+const modalImage = document.getElementById("modalImage");
+
+// Get the close button element
+const closeBtn = document.getElementsByClassName("close")[0];
+
+// Select all preview images
+const previewImages = document.querySelectorAll("img[id='previewImage']");
+
+// Add click event to each preview image
+previewImages.forEach(image => {
+  image.addEventListener("click", function () {
+    modal.style.display = "flex"; // Show the modal as flex for center alignment
+    modalImage.src = this.src;
+  });
+});
+
+// Close the modal when the close button is clicked
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+}
+
+// Close the modal when clicking outside the image
+modal.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
